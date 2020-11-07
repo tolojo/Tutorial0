fed6a09eeb221f22937dffca7a54610393ad43cb
tolojo <73295309+tolojo@users.noreply.github.com>
2020-11-07 16:00:18 +0000

    Initial Commit

---
+++ src/main/java/com/example/Projeto_0/Projeto0Application.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto0Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Projeto_0/controllers/GreeterController.java
@@ -0,0 +1,33 @@
+package com.example.Projeto_0.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping(path="/api/greeter")
+public class GreeterController {
+
+
+ private Logger logger = LoggerFactory.getLogger(GreeterController.class);
+ @GetMapping(path = "", produces= MediaType.APPLICATION_JSON_VALUE)
+ public String getGreeting() {
+ logger.info("Saying Hello to the world");
+ return "Hello World";
+ }
+
+ @GetMapping(path = "{name}",
+ produces= MediaType.APPLICATION_JSON_VALUE)
+ public String getGreeting(@PathVariable("name") String name) {
+ logger.info("Saying Hello to "+name);
+ return "Hello "+name;
+ }
+
+
+
+
+}
+
